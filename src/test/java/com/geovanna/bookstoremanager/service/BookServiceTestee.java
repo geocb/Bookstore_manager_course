ee638547255c1af99c558ef146fd35ab66138768
Iury Brito <iury.brito@griaule.com>
2021-09-22 10:03:20 -0300

    Created unit test for book not found exception

--- src/test/java/com/geovanna/bookstoremanager/service/BookServiceTestee.java
+++ src/test/java/com/geovanna/bookstoremanager/service/BookServiceTestee.java
@@ -4,6 +4,7 @@ import com.geovanna.bookstoremanager.dto.AuthorDTO;
 import com.geovanna.bookstoremanager.dto.BookDTO;
 import com.geovanna.bookstoremanager.dto.MessageResponseDTO;
 import com.geovanna.bookstoremanager.entity.Book;
+import com.geovanna.bookstoremanager.exception.BookNotFoundException;
 import com.geovanna.bookstoremanager.mapper.BookMapper;
 import com.geovanna.bookstoremanager.repository.BookRepository;
 import org.junit.jupiter.api.Assertions;
@@ -13,6 +14,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class BookServiceTest {
@@ -30,11 +34,19 @@ class BookServiceTest {
 
     @Test
     void testCreate() {
-        when(bookMapper.toModel(any())).thenReturn(new Book(Long.valueOf(1), "name", Integer.valueOf(0), Integer.valueOf(0), "isbn", "publisherName", null));
+        when(bookMapper.toModel(any())).thenReturn(new Book(1L, "name", 0, 0, "isbn", "publisherName", null));
 
-        MessageResponseDTO result = bookService.create(new BookDTO(Long.valueOf(1), "name", Integer.valueOf(0), Integer.valueOf(0), "isbn", "publisherName", new AuthorDTO(Long.valueOf(1), "name", Integer.valueOf(0))));
+        MessageResponseDTO result = bookService.create(new BookDTO(1L, "name", 0, 0, "isbn", "publisherName", new AuthorDTO(Long.valueOf(1), "name", Integer.valueOf(0))));
         Assertions.assertEquals(null, result);
     }
-}
+    @Test
+    void whenGivesUnexistingIdThenNotFindThrowAnException() {
+        long invalidId = 10L;
 
+        when(bookRepository.findById(invalidId))
+                .thenReturn(Optional.ofNullable(any(Book.class)));
+
+        assertThrows(BookNotFoundException.class, () -> bookService.findById(invalidId));
+    }
+}
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
